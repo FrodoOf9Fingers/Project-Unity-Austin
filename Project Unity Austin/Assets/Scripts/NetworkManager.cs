44f27694e82fc50385370457e0ac3c922bc3d027
FrodoOf9Fingers <joseph.r.hales@gmail.com>
2015-10-24 13:20:54 -0600

    Started creating and testing network capabilities in Unity. Discovered that some items were depreciated (NetworkView), and more research into networking is required.

---
+++ Project Unity Austin/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+    private const string typeName = "Unity Austin";
+    private const string gameName = "Austin";
+    private HostData[] hostList;
+
+    //If needed, we can create a local Master Server
+    //MasterServer.ipAddress = "127.0.0.1";
+
+    /*Start the server for 2 players on port 25000*/
+    void StartServer () {
+        //TODO: allow for multiple users, talk about and find a good port to use
+        Network.InitializeServer(2, 25000, !Network.HavePublicAddress());
+        MasterServer.RegisterHost(typeName, gameName);
+	}
+
+    /*Log a message if the server starts correctly*/
+    void OnServerInitialized()
+    {
+        Debug.Log("Server Initializied");
+    }
+
+    /*Create a button if the user is neither a client nor a server to start the server*/
+    void OnGUI()
+    {
+        if (!Network.isClient && !Network.isServer)
+        {
+            if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+                StartServer();
+
+            if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
+                RefreshHostList();
+
+            if (hostList != null)
+            {
+                for (int i = 0; i < hostList.Length; i++)
+                {
+                    if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
+                        JoinServer(hostList[i]);
+                }
+            }
+        }
+    }
+    private void RefreshHostList()
+    {
+        MasterServer.RequestHostList(typeName);
+    }
+
+    void OnMasterServerEvent(MasterServerEvent msEvent)
+    {
+        if (msEvent == MasterServerEvent.HostListReceived)
+            hostList = MasterServer.PollHostList();
+    }
+    private void JoinServer(HostData hostData)
+    {
+        Network.Connect(hostData);
+    }
+
+    void OnConnectedToServer()
+    {
+        Debug.Log("Server Joined");
+    }
+}
