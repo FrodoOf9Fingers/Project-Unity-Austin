bc3cc15a3f9ce846517f5fa56dfcc83e4f6939f3
Joseph Hales <joseph.r.hales@gmail.com>
2015-11-06 14:55:05 -0700

    Fixed Commit

---
+++ Project Unity Austin/Assets/Scripts/PlayerNetworkMover.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PlayerNetworkMover : Photon.MonoBehaviour
+{
+
+    public delegate void Respawn(float time);
+    public event Respawn RespawnMe;
+
+    Vector3 position;
+    Quaternion rotation;
+    float smoothing = 10f;
+    
+    void Start()
+    {
+
+        if (photonView.isMine)
+        {
+            GetComponent<RigidbodyFirstPersonController>().enabled = true;
+            GetComponent<Rigidbody>().useGravity = true;
+        }
+        else
+        {
+            StartCoroutine("UpdateData");
+        }
+    }
+
+    IEnumerator UpdateData()
+    {
+        while (true)
+        {
+            transform.position = Vector3.Lerp(transform.position, position, Time.deltaTime * smoothing);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * smoothing);
+            yield return null;
+        }
+    }
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            position = (Vector3)stream.ReceiveNext();
+            rotation = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
